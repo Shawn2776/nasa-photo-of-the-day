e6ebbd837d623e33ad1a8918098197c5eb395f81
Shawn <shawn.harrington2776@gmail.com>
2020-11-09 14:03:20 -0800

    finished assignemnt, day 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
+export default function App() {
+
+  const [ nasaData, setNasaData ] = useState([])
+// https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+// https://api.nasa.gov/planetary/apod?api_key=mEYe2vfodAXchwQgESvVzD8bg8LYoFJludunywaH
+  useEffect(() => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14')
+    .then((res) => {
+      setNasaData(res.data)
+    })
+    .catch((err) => {
+      alert('Help')
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="app">
+      <h1>{nasaData.title}</h1>
+      <div className="image"><img src={nasaData.url} alt={nasaData.copyright} width="300"></img></div>
+      <h2>{nasaData.date}</h2>
+      <div className="explanation">{nasaData.explanation}</div>
+
     </div>
   );
 }
-
-export default App;
